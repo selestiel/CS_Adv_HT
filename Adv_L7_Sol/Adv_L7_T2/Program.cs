7e804fcf78e60ce4c59a74b3f3e37152c304c5a5
Selestiel <selarch2018@gmail.com>
2020-03-09 22:13:59 +0200

    L7 Upd
    addon

--- Adv_L7_Sol/Adv_L7_T2/Program.cs
+++ Adv_L7_Sol/Adv_L7_T2/Program.cs
@@ -11,7 +11,21 @@ namespace Adv_L7_T2
     {
         static void Main(string[] args)
         {
-            
+            FileStream stream = new FileStream("MyFile", FileMode.OpenOrCreate, FileAccess.Write,FileShare.Write);
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine("Fully implemented dominator system based on few age 3225 modifications.");
+            writer.WriteLine("And thus the new Age has begun evolving entire world in some thing new, something chaotic!");
+            writer.Close();
+            stream.Close();
+            if (File.Exists("MyFile") == true)
+            {
+                FileStream stream1 = new FileStream("MyFile", FileMode.Open, FileAccess.Read,FileShare.Read);
+                StreamReader reader = new StreamReader(stream1);
+                Console.WriteLine(reader.ReadToEnd());
+                reader.Close();
+                stream1.Close();
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Adv_L7_Sol/Adv_L7_T3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adv_L7_T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
