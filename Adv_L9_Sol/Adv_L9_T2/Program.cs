2c194465bdc4107eb9d4160232f59b84cd043653
Selestiel <selarch2018@gmail.com>
2020-04-02 20:39:21 +0300

    L9 upd(last)

--- Adv_L9_Sol/Adv_L9_T2/Program.cs
+++ Adv_L9_Sol/Adv_L9_T2/Program.cs
@@ -10,6 +10,93 @@ namespace Adv_L9_T2
     {
         static void Main(string[] args)
         {
+            Director director = new Director();
+            director.Info();
+            Manager manager = new Manager();
+            manager.Info();
+            Programmer programmer = new Programmer();
+            programmer.Info();
+
+            Console.ReadKey();
+
+        }
+    }
+    class Director
+    {
+        [AccessLevel(AccessLevelAttribute.User.Director)]
+        public void Info()
+        {
+            AccessLevelAttribute attribute = new AccessLevelAttribute(AccessLevelAttribute.User.Director);
+            Console.WriteLine("Director: Access Level: ");
+            attribute.GetLevel();
+            
+        }
+    }
+    class Manager
+    {
+        [AccessLevel(AccessLevelAttribute.User.Manager)]
+        public void Info()
+        {
+            AccessLevelAttribute attribute = new AccessLevelAttribute(AccessLevelAttribute.User.Manager);
+            Console.WriteLine("Manager: Access Level: ");
+            attribute.GetLevel();
+
+        }
+    }
+    class Programmer
+    {
+        [AccessLevel(AccessLevelAttribute.User.Programmer)]
+        public void Info()
+        {
+            AccessLevelAttribute attribute = new AccessLevelAttribute(AccessLevelAttribute.User.Programmer);
+            Console.WriteLine("Programmer: Access Level: ");
+            attribute.GetLevel();
+
+        }
+    }
+    class AccessLevelAttribute : System.Attribute
+    {
+        AccessLevelE level { get; set; }
+        enum AccessLevelE
+        {
+            SSS,
+            SS,
+            S,
+            A,
+            B,
+            C,
+            D,
+            E,
+            F
+        };
+        public enum User
+        {
+            Manager, Programmer, Director
+        };
+        public void GetLevel()
+        {
+            Console.WriteLine(this.level);
+        }
+        private AccessLevelE AccsessLevel(User user)
+        { 
+            switch (user)
+            {
+                case User.Director:
+                    level = AccessLevelE.A;
+                    break;
+                case User.Manager:
+                    level = AccessLevelE.E;
+                    break;
+                case User.Programmer:
+                    level = AccessLevelE.SSS;
+                    break;
+            }
+            return level;
+        }
+        public AccessLevelAttribute(User u)
+        {
+
+            this.AccsessLevel(u);
         }
     }
 }
