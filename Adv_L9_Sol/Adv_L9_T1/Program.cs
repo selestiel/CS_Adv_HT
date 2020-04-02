1ea1804cce0460d13564fb34a25e15d4a8e04e5f
Selestiel <selarch2018@gmail.com>
2020-04-02 17:10:41 +0300

    Upd L9

--- Adv_L9_Sol/Adv_L9_T1/Program.cs
+++ Adv_L9_Sol/Adv_L9_T1/Program.cs
@@ -12,6 +12,33 @@ namespace Adv_L9_T1
     {
         static void Main(string[] args)
         {
+            MyClass myClass = new MyClass("loki",99);
+            
+            myClass.Info();
+            myClass.Info2();
+            Console.ReadKey();
+        }
+
+    }
+    public class MyClass
+    {
+        private string name { get; set; }
+        private int age { get; set; }
+        [Obsolete]
+        public void Info()
+        {
+            Console.WriteLine(@"name {0} age {1}",name,age);
+        }
+
+        [Obsolete("OLD method implementation use new method please",true)]
+        public void Info2()
+        {
+            Console.WriteLine("name2 " + name + " age2 " + age);
+        }
+        public MyClass(string n,int a)
+        {
+            name = n;
+            age = a;
         }
     }
 }
